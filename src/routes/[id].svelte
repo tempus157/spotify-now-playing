9441634a1c6109468e1c4623cd7cd42a390cb85c
Park Changyong <tempus157@gmail.com>
2022-04-04 14:16:25 +0900

    Make Player UI

--- src/routes/[id].svelte
+++ src/routes/[id].svelte
@@ -5,3 +5,61 @@
     <div class="artist">Artist</div>
   </div>
 </div>
+
+<style lang="scss">
+  @import url("https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@700&display=swap");
+
+  .container {
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    flex-direction: row;
+  }
+
+  .album-art {
+    position: relative;
+    width: 100vh;
+    height: 100vh;
+  }
+
+  .info {
+    width: calc(100vw - 100vh);
+    padding-bottom: 10vh;
+    align-self: center;
+    overflow: hidden;
+    white-space: nowrap;
+  }
+
+  .name {
+    width: fit-content;
+    padding-left: 25vh;
+    color: white;
+    font-family: "Noto Sans KR", sans-serif;
+    font-size: 30vh;
+    line-height: 120%;
+    text-shadow: 0 0 1px #000, 0 0 2px #000;
+  }
+
+  .artist {
+    width: fit-content;
+    padding-left: 25vh;
+    color: gray;
+    font-family: "Noto Sans KR", sans-serif;
+    font-size: 20vh;
+    line-height: 120%;
+    text-shadow: 0 0 1px #000, 0 0 2px #000;
+  }
+
+  .scroll {
+    animation: slide 100s linear infinite;
+  }
+
+  @keyframes slide {
+    from {
+      transform: translateX(calc(100vw - 100vh));
+    }
+    to {
+      transform: translateX(-100%);
+    }
+  }
+</style>
