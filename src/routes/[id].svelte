eaf52a0eccab224c904e73060f6d40aac7d4a0d3
Park Changyong <tempus157@gmail.com>
2022-04-04 14:07:22 +0900

    Add music player ui

---
+++ src/routes/[id].svelte
@@ -0,0 +1,7 @@
+<div class="container">
+  <img src="/static/favicon.png" alt="Album art" class="album-art" />
+  <div class="info">
+    <div class="name">Name</div>
+    <div class="artist">Artist</div>
+  </div>
+</div>
