d18fa02fb9845f270ff3f7069e3bdbccbe6f5862
Park Changyong <tempus157@gmail.com>
2022-04-05 23:37:52 +0900

    Refactor cssVariable library

--- src/libs/cssVariable.ts
+++ src/libs/cssVariable.ts
@@ -1,4 +1,6 @@
-export default function cssVariable(...variables: [string, string][]) {
+type Variable = [string, string];
+
+export default function cssVariable(...variables: Variable[]) {
   document.documentElement.style.cssText = variables
     .map(([name, value]) => `--${name}:${value}`)
     .join(";");
