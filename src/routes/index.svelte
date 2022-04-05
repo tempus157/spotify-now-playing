5a5e25ce25e194c0d0534cf65722495e7fe5ffb4
Park Changyong <tempus157@gmail.com>
2022-04-05 14:05:22 +0900

    Merge index and config

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,15 @@
 <script lang="ts">
   import Title from "$components/Title.svelte";
+
+  let nameColor = "#8ab4f8";
+  let artistColor = "#ffffff";
+  let backgroundColor = "#202124";
+  let backgroundOpacity = 25;
+  let cornerRounding = 40;
+
+  function hello() {
+    alert("Hello");
+  }
 </script>
 
 <svelte:head>
@@ -7,26 +17,61 @@
 </svelte:head>
 
 <div>
-  <a href="/api/auth">
-    <button class="connect">
-      <img src="/static/spotify.png" alt="Spotify Logo" />
-      <span>Connect Spotify</span>
-    </button>
-  </a>
+  {#if true}
+    <a href="/api/auth">
+      <button class="connect">
+        <img src="/static/spotify.png" alt="Spotify Logo" />
+        <span>Connect Spotify</span>
+      </button>
+    </a>
+  {:else}
+    <form on:submit|preventDefault={hello}>
+      <label>
+        <span>Song Name Color</span>
+        <input type="color" bind:value={nameColor} />
+      </label>
+
+      <label>
+        <span>Artist Color</span>
+        <input type="color" bind:value={artistColor} />
+      </label>
+
+      <label>
+        <span>Background Color</span>
+        <input type="color" bind:value={backgroundColor} />
+      </label>
+
+      <label>
+        <span>Background Opacity</span>
+        <input type="range" min="0" max="100" bind:value={backgroundOpacity} />
+        <span>{backgroundOpacity}%</span>
+      </label>
+
+      <label>
+        <span>Corner Rounding</span>
+        <input type="range" min="0" max="100" bind:value={cornerRounding} />
+        <span>{cornerRounding}%</span>
+      </label>
+
+      <button>Save Config</button>
+    </form>
+
+    <button>Copy URL</button>
+  {/if}
 </div>
 
 <style>
-  :root {
-    --accent: #1db954;
+  label {
+    display: block;
   }
 
   .connect {
     padding: 10px;
     display: flex;
     align-items: center;
-    background-color: var(--accent);
-    border-radius: 100px;
+    background-color: #1db954;
     border-color: transparent;
+    border-radius: 100px;
   }
 
   .connect img {
